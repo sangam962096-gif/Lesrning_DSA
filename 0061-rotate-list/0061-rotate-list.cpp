d1debd5dccfa20aa44b6f41f356c8afb1e7ffabc
Sangam Yadav <sangam962096@gmail.com>
2026-08-26 22:40:29 +0530

    Time: 0 ms (100%), Space: 16.5 MB (32.89%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ ListNode* findnthnode( ListNode* head , int k ){
+    
+    ListNode* temp = head;
+    for( int i = 1 ; i < k; i++){
+        temp = temp->next;
+
+    }
+    return temp;
+ }
+   
+    ListNode* rotateRight(ListNode* head, int k) {
+        if( head == NULL || head->next == NULL) return head;
+        ListNode* tail = head;
+    int len = 1;
+        while( tail->next != NULL){
+            tail = tail->next;
+            len +=1;
+        }
+        if( k % len == 0) return head;
+
+        k = k % len ;
+         
+         // tail ko head se attach kar denge
+         tail->next = head;
+         ListNode* newlastnode = findnthnode( head , len - k);
+
+        head = newlastnode->next ;
+         newlastnode->next = NULL;
+         return head;
+
+    }
+};
